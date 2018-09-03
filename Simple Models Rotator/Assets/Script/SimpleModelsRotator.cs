b59f03137e8ff15ac88fbb1b738016d463de821f
Moch Iqbal Fauzi <mochiqbalfauzi@midieland.com>
2018-09-03 12:47:46 +0700

    Add Functionality

---
+++ Simple Models Rotator/Assets/Script/SimpleModelsRotator.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class SimpleModelsRotator : MonoBehaviour {
+
+    public Transform model;
+    public SimpleModelsRotator_Preset preset;
+
+    private Camera mainCamera;
+
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (preset.rotating.rightMouseButtonInput)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+
+                Rotate();
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            Rotate();
+        }
+
+        Zoom();
+    }
+
+    private void Rotate()
+    {
+        float rotX = Input.GetAxis("Mouse X");
+        float rotY = Input.GetAxis("Mouse Y");
+
+        Vector3 inputRot = new Vector3(rotY, rotX, 0);
+        Vector3 currentRot = new Vector3(model.rotation.eulerAngles.x, model.rotation.eulerAngles.y, model.rotation.eulerAngles.z);
+        Quaternion newRot = Quaternion.Euler(currentRot + inputRot);
+        model.rotation = newRot;
+    }
+
+    private void Zoom()
+    {
+        float zoomInput = Input.GetAxis("Mouse ScrollWheel") * preset.zooming.zoomSpeed;
+
+        float zoomValue = mainCamera.fieldOfView - zoomInput;
+        zoomValue = Mathf.Clamp(zoomValue, preset.zooming.zoomMin, preset.zooming.zoomMax);
+
+        mainCamera.fieldOfView = zoomValue;
+    }
+}
---
+++ Simple Models Rotator/Assets/Script/SimpleModelsRotator_Preset.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class SimpleModelsRotator_Preset : ScriptableObject {
+
+    public Rotating rotating = new Rotating();
+    public Zooming zooming = new Zooming();
+
+    [System.Serializable]
+    public class Rotating {
+        public bool rightMouseButtonInput = false;
+    }
+
+	[System.Serializable]
+    public class Zooming {
+        public float zoomSpeed = 10;
+        public float zoomMin = 30;
+        public float zoomMax = 60;
+    }
+}
